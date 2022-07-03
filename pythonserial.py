d057b6b2f5c69f2e293c3bc65eed64bcf51b66f8
Charlie Boardman <cboardman2@gmail.com>
2022-07-03 16:26:59 -0700

    Rewrote serial communication between python script and arduino script to only send when circuit is closed. Python also now creates a CSV file of timestamp and circuit closed

--- pythonserial.py
+++ pythonserial.py
@@ -1,14 +1,27 @@
+import time
 import serial
 
+
+#port is set to COM5 for my windows PC. When testing on linux laptop or macbook, will need to change this
 serialPort = serial.Serial(port = "COM5", baudrate=9600,
                            bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
 count = 0
+arr = [[],[]]
+starttime = time.time()
 
 #serialPort.open()
 while True:
-    line =serialPort.readline() 
-    if line:
-        count = count + 1
-    if count == 1:
-        print(line)
-        count = 0
+    if serialPort.in_waiting > 0:
+        line = serialPort.readline().strip()
+        arr[0].append(time.time()-starttime)
+        arr[1].append(line)
+        
+    if time.time() - starttime >= 10:
+        break
+    
+f = open('csvfile.csv','w')
+
+for row in range(len(arr[0])):
+    f.write(str(arr[0][row])+','+str(int(arr[1][row]))+'\n')
+    
+f.close()
--- serial_water_main/serial_water_main.ino
+++ serial_water_main/serial_water_main.ino
@@ -4,27 +4,26 @@ void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(2, INPUT_PULLUP);
-  Serial.begin(9600);
+  Serial.begin(9600);  
 
 }
 
+
 // the loop function runs over and over again forever
 void loop() {
-
-  delay(1000);
+  
   int sensorVal = digitalRead(2);
-
   if (sensorVal == HIGH) {
 
-  digitalWrite(LED_BUILTIN, LOW);
-    Serial.println('0');
-    Serial.flush();
+  //digitalWrite(LED_BUILTIN, LOW);
+    //Serial.println('0');
+    //Serial.flush();
 
   } if (sensorVal == LOW) {
 
   digitalWrite(LED_BUILTIN, HIGH);
-    Serial.println("What about\nMultiple\nLines?");
+    Serial.println('1');
     Serial.flush();
   }
-
+  
 }
